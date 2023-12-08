ba54297516019acae4c9409c6046d475dedc77a3
holykremowka2137 <victoria2137@tutanota.com>
2023-12-08 10:13:10 +0100

    smaller or bigger changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,6 +47,7 @@ alias gcommit "git commit -m"
 alias gpush "git push"
 alias gpull "git pull"
 alias gmerge "git merge"
+alias gstatus "git status"
 
 # mirrors
 alias mirror "sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
