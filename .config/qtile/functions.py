92abd2a2d931b72b1d59fe7176b68f7045ae2221
holykremowka2137 <victoria2137@tutanota.com>
2024-02-15 20:56:05 +0100

    1 function is commented out

--- .config/qtile/functions.py
+++ .config/qtile/functions.py
@@ -1,8 +1,8 @@
-from libqtile import hook, qtile
+#from libqtile import hook, qtile
 from libqtile.lazy import lazy
-from libqtile.utils import send_notification
+#from libqtile.utils import send_notification
 
-from colors import catppuccin
+#from colors import catppuccin
 
 @lazy.layout.function
 def change_gaps(layout, step):
@@ -20,13 +20,13 @@ def minimize_all(qtile):
         if hasattr(win, "toggle_minimize"):
             win.toggle_minimize()
 
-@hook.subscribe.layout_change
-def layout_change(layout, group):
-    global bar_clr 
-    if qtile.current_layout.name == "max":
-        send_notification(f"{layout.name}", f"Is now on group {group.label}")
-        bar_clr = catppuccin["mantle"]
-    if qtile.current_layout.name == "monadtall":
-        bar_clr = "#00000000"
-    elif qtile.current_layout.name == "columns":
-        bar_clr = "#00000000"
+#@hook.subscribe.layout_change
+#def layout_change(layout, group):
+#    global bar_clr 
+#    if qtile.current_layout.name == "max":
+#        send_notification(f"{layout.name}", f"Is now on group {group.label}")
+#        bar_clr = catppuccin["mantle"]
+#    if qtile.current_layout.name == "monadtall":
+#        bar_clr = "#00000000"
+#    elif qtile.current_layout.name == "columns":
+#        bar_clr = "#00000000"
