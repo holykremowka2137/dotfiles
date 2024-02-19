a7e27ca4d1ef476f6dda96959d3b324026cae03a
holykremowka2137 <victoria2137@tutanota.com>
2024-02-19 20:58:00 +0100

    small changes

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,15 +1,12 @@
 function fish_right_prompt
     if test $CMD_DURATION
         set -g duration (echo "$CMD_DURATION 1000" | awk '{printf "%.0fs", $1 / $2}')
-        if test $duration = "1s"
-            echo "" 
-        else if test $duration = "0s"
+        if test $duration = "1s" || test $duration = "0s"
             echo "" 
         else
             set_color -o yellow
-            echo \[$duration\]
+            echo "[$duration]"
         end
     end
-    fish_vcs_prompt
     fish_default_mode_prompt
 end
