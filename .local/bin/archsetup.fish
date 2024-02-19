e1d9cccd453a03c8670901ecf21447af93e23603
holykremowka2137 <victoria2137@tutanota.com>
2024-02-19 20:56:14 +0100

    Re-thinking evolution. Was Darwin wrong?

--- .local/bin/archsetup.fish
+++ .local/bin/archsetup.fish
@@ -4,7 +4,7 @@ sudo pacman -Syyu
 
 function which_cpu
     echo
-    echo (set_color -oi green)"What CPU do you have?" 
+    echo (set_color -oi green)"Which CPU do you have?" 
     set_color normal
     echo (set_color green)"1. AMD"
     echo (set_color green)"2. Intel"
@@ -12,11 +12,9 @@ function which_cpu
     read choice
     switch $choice
     case 1
-        sudo pacman -S amd-ucode
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm amd-ucode
     case 2
-        sudo pacman -S intel-ucode
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm intel-ucode
     case '*' 
         echo
         echo (set_color -oi green)"Are you sure about that?"
@@ -34,12 +32,10 @@ function xvswl
     read choice
     switch $choice
     case 1
-        sudo pacman -S xorg xorg-xinit xclip picom redshift xdotool flameshot 
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm xorg xorg-xinit xclip picom redshift xdotool flameshot 
     case 2
-        sudo pacman -S wayland wlroots pywlroots python-pywayland python-xkbcommon xorg-xwayland wl-clipboard gammastep ydotool xdg-desktop-portal-wlr grim slurp
+        sudo pacman -S --noconfirm wayland wlroots pywlroots python-pywayland python-xkbcommon xorg-xwayland wl-clipboard gammastep ydotool xdg-desktop-portal-wlr grim slurp
         echo
-        echo (set_color -o green)"Done!"
     case '*'
         echo
         echo (set_color -oi green)"Are you sure about that?"
@@ -51,30 +47,28 @@ function packages
     echo
     echo (set_color -oi green)"Installing fonts..."
     set_color normal
-    sudo pacman -S ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono ttf-nerd-fonts-symbols-common noto-fonts-extra noto-fonts-emoji noto-fonts-cjk 
-    echo (set_color -o green)"Done!"
+    sudo pacman -S --noconfirm ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono ttf-nerd-fonts-symbols-common noto-fonts-extra noto-fonts-emoji noto-fonts-cjk 
 
     echo (set_color -oi green)"Installing apps..."
     set_color normal
-    sudo pacman -S vivaldi thunderbird krita ristretto libreoffice-still okular xed steam deluge-gtk psensor alacritty kitty gparted file-roller file-roller fcitx-im fcitx-mozc fcitx-configtool lxappearance rofi rofi-emoji networkmanager hunspell-en_US bluez bluez-utils signal-desktop totem obs-studio kvantum
-    echo (set_color -o green)"Done!"
+    sudo pacman -S --noconfirm vivaldi thunderbird krita ristretto libreoffice-still xed steam deluge-gtk psensor gparted file-roller file-roller fcitx-im fcitx-mozc fcitx-configtool lxappearance rofi rofi-emoji networkmanager hunspell-en_US bluez bluez-utils totem obs-studio kvantum
 
     echo (set_color -oi green)"Installing command-line apps..."
     set_color normal
-    sudo pacman -S neovim yt-dlp python-pycryptodome mpv python-pip python-pygame man-db sagemath ufw ffmpeg brotli python-mutagen python-pyxattr fish p7zip unrar tar rsync neofetch uwufetch htop exfat-utils fuse-exfat ntfs-3g flac jasper aria2 curl wget reflector plocate unzip zip dunst cmatrix cython python-jupymake eza
-    echo (set_color -o green)"Done!"
+    sudo pacman -S --noconfirm neovim yt-dlp python-pycryptodome mpv python-pip python-pygame man-db sagemath ufw ffmpeg brotli python-mutagen python-pyxattr fish p7zip unrar tar rsync neofetch uwufetch htop exfat-utils fuse-exfat ntfs-3g flac jasper aria2 curl wget reflector plocate unzip zip dunst cmatrix cython python-jupymake eza
+
+    # deep, dark fantasy
 
     echo (set_color -oi green)"Installing pipewire..."
     set_color normal
-    sudo pacman -S pipewire pipewire-pulse pipewire-jack wireplumber helvum easyeffects calf yelp pipewire-alsa alsa-utils lib32-pipewire pipewire-audio lib32-pipewire-jack
-    echo (set_color -o green)"Done!"
+    sudo pacman -S --noconfirm pipewire pipewire-pulse pipewire-jack wireplumber helvum easyeffects calf yelp pipewire-alsa alsa-utils lib32-pipewire pipewire-audio lib32-pipewire-jack
 
     echo (set_color -oi green)"Finished!"
 end
 
 function file_manager
     echo 
-    echo (set_color -oi green)"What GUI file manager do you want?"
+    echo (set_color -oi green)"Which GUI file manager do you want?"
     set_color normal
     echo (set_color green)"1. Thunar"
     echo (set_color green)"2. Nemo"
@@ -86,27 +80,59 @@ function file_manager
     case 1
         echo (set_color -oi green)"Installing Thunar..."
         set_color normal
-        sudo pacman -S thunar catfish gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler gobject-introspection intltool gvfs-mtp ffmpegthumbnailer
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm thunar catfish gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler gobject-introspection intltool gvfs-mtp ffmpegthumbnailer
     case 2
         echo (set_color -oi green)"Installing Nemo..."
         set_color normal
-        sudo pacman -S nemo cinnamon-translations nemo-image-converter nemo-preview
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm nemo cinnamon-translations nemo-image-converter nemo-preview
     case 3
         echo (set_color -oi green)"Installing Yazi"
         set_color normal
-        sudo pacman -S yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide
     case 4
         echo (set_color -oi green)"Installing all of them..."
         set_color normal
-        sudo pacman -S thunar catfish gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler gobject-introspection intltool gvfs-mtp ffmpegthumbnailer nemo cinnamon-translations nemo-image-converter nemo-preview yazi unarchiver jq poppler fd ripgrep fzf zoxide
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm thunar catfish gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler gobject-introspection intltool gvfs-mtp ffmpegthumbnailer
+        sudo pacman -S --noconfirm nemo cinnamon-translations nemo-image-converter nemo-preview
+        sudo pacman -S --noconfirm yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide
     case '*'
         echo 
         echo (set_color -oi green)"Are you sure about that?"
         file_manager
+    end
+end
+
+function terminals
+    echo 
+    echo (set_color -oi green)"Which terminal emulator do you want?"
+    set_color normal
+    echo (set_color green)"1. Foot"
+    echo (set_color green)"2. Alacritty"
+    echo (set_color green)"3. Kitty"
+    echo (set_color green)"4. All of 'em"
+    echo (set_color -o yellow)"Your choice: " 
+    read choice
+    switch $choice
+    case 1
+        echo (set_color -oi green)"Installing Foot..."
+        set_color normal
+        sudo pacman -S --noconfirm foot
+    case 2
+        echo (set_color -oi green)"Installing Alacritty..."
+        set_color normal
+        sudo pacman -S --noconfirm alacritty
+    case 3
+        echo (set_color -oi green)"Installing Kitty..."
+        set_color normal
+        sudo pacman -S --noconfirm kitty
+    case 4
+        echo (set_color -oi green)"Installing all of them..."
+        set_color normal
+        sudo pacman -S --noconfirm alacritty kitty foot
+    case '*'
+        echo 
+        echo (set_color -oi green)"Are you sure about that?"
+        terminals
     end
 end
 
@@ -116,7 +142,7 @@ function Neovim
     read choice
     switch $choice
     case y
-        sudo pacman -S --needed neovim fd ripgrep npm 
+        sudo pacman -S --noconfirm --needed neovim fd ripgrep npm 
         git clone https://www.github.com/holykremowka2137/init.lua.git ~/.config/nvim && nvim
         ln -s .local/state/nvim/swap/ ~/nvim-swap
         echo (set_color -oi green)"Configuration installed!"
@@ -137,7 +163,7 @@ function doomemacs
     read choice
     switch $choice
     case y
-        sudo pacman -S --needed emacs-nativecomp fd ripgrep npm 
+        sudo pacman -S --noconfirm --needed emacs-nativecomp fd ripgrep npm 
         git clone https://www.github.com/hlissner/doom-emacs ~/.emacs.d
         ~/.emacs.d/bin/doom install
         echo (set_color -oi green)"Doom Emacs installed!"
@@ -158,7 +184,7 @@ function dotfiles
     read choice
     switch $choice
     case y
-        sudo pacman -S stow
+        sudo pacman -S --noconfirm stow
         git clone https://www.github.com/holykremowka2137/dotfiles.git ~/Repos/dotfiles/
         stow -v --target="$HOME"/.config/ ~/Repos/dotfiles/.config/.
         mkdir ~/.local/bin/
@@ -180,7 +206,7 @@ function install_paru
     read choice
     switch $choice
     case y
-        sudo pacman -S --needed base-devel git
+        sudo pacman -S --noconfirm --needed base-devel git
         git clone https://aur.archlinux.org/paru.git
         cd paru
         makepkg -si
@@ -204,7 +230,7 @@ function install_aur
     read choice
     switch $choice
     case y
-        paru -S catppuccin-gtk-theme-mocha catppuccin-cursors-mocha papirus-folders-catppuccin-git qtile-extras brillo auto-cpufreq librewolf-bin kvantum-theme-catppuccin-git
+        paru -S --noconfirm catppuccin-gtk-theme-mocha catppuccin-cursors-mocha papirus-folders-catppuccin-git qtile-extras brillo auto-cpufreq librewolf-bin kvantum-theme-catppuccin-git
         sudo auto-cpufreq --install
         echo (set_color -oi green)"AUR packages built."
     case n
@@ -222,7 +248,7 @@ function install_flatpak
     read choice
     switch $choice
     case y
-        sudo pacman -S flatpak flatpak-xdg-utils
+        sudo pacman -S --noconfirm flatpak flatpak-xdg-utils
         echo (set_color -oi green)"Flatpak installed."
         set_color normal
     case n
@@ -248,31 +274,25 @@ function DM
     case 1
         echo (set_color -oi green)"Installing Ly and uninstalling LightDM..."
         set_color normal
-        sudo pacman -S ly && sudo pacman -Rns lightdm
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm ly && sudo pacman -Rns --noconfirm lightdm
         echo (set_color -oi green)"Enabling Ly..."
         set_color normal
         sudo systemctl enable ly
-        echo (set_color -o green)"Done!"
     case 2
         echo (set_color -oi green)"Installing SDDM and uninstalling LightDM..."
         set_color normal
-        sudo pacman -S sddm && sudo pacman -Rns lightdm
-        echo (set_color -o green)"Done!"
+        sudo pacman -S --noconfirm sddm && sudo pacman -Rns --noconfirm lightdm
         echo (set_color -oi green)"Enabling SDDM..."
         set_color normal
         sudo systemctl enable sddm
-        echo (set_color -o green)"Done!"
     case 3
         echo (set_color -oi green)"Enabling LightDM..."
         set_color normal
         sudo systemctl enable lightdm
-        echo (set_color -o green)"Done!"
     case 4
         echo (set_color -oi green)"Uninstalling LightDM..."
         set_color normal
-        sudo pacman -Rns lightdm
-        echo (set_color -o green)"Done! Enjoy your bloat-free system!"
+        sudo pacman -Rns --noconfirm lightdm
     case '*'
         echo 
         echo (set_color -oi green)"Are you sure about that?"
@@ -285,23 +305,19 @@ function enabling
     echo (set_color -oi green)"Enabling Bluetooth..."
     set_color normal
     sudo systemctl enable bluetooth
-    echo (set_color -o green)"Done!"
 
     echo (set_color -oi green)"Enabling 'locate' & adding user to groups..."
     set_color normal
     sudo updatedb
     sudo usermod -aG locate,wheel,video,tty,dbus,input "$USER"
-    echo (set_color -o green)"Done!"
 
     echo (set_color -oi green)"Enabling autostart.sh..."
     set_color normal
     chmod +x ~/.config/qtile/autostart.sh
-    echo (set_color -o green)"Done!"
 
-    echo (set_color -oi green)"Changing colors of Papirus Icon Theme..."(set_color normal)
+    echo (set_color -oi green)"Changing colors of Papirus Icon Theme..."
     set_color normal
     papirus-folders -C cat-mocha-pink --theme Papirus-Dark
-    echo (set_color -o green)"Done!"
 end
 
 function rebooting
@@ -325,6 +341,7 @@ which_cpu
 xvswl
 packages
 file_manager
+terminals
 Neovim
 doomemacs
 dotfiles
