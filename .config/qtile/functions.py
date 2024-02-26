6bdf45ff511148cbdba50bc12764ad9a8838abff
holykremowka2137 <victoria2137@tutanota.com>
2024-02-26 02:56:32 +0100

    deleted dead function

--- .config/qtile/functions.py
+++ .config/qtile/functions.py
@@ -1,8 +1,4 @@
-#from libqtile import hook, qtile
 from libqtile.lazy import lazy
-#from libqtile.utils import send_notification
-
-#from colors import catppuccin
 
 @lazy.layout.function
 def change_gaps(layout, step):
@@ -19,14 +15,3 @@ def minimize_all(qtile):
     for win in qtile.current_group.windows:
         if hasattr(win, "toggle_minimize"):
             win.toggle_minimize()
-
-#@hook.subscribe.layout_change
-#def layout_change(layout, group):
-#    global bar_clr 
-#    if qtile.current_layout.name == "max":
-#        send_notification(f"{layout.name}", f"Is now on group {group.label}")
-#        bar_clr = catppuccin["mantle"]
-#    if qtile.current_layout.name == "monadtall":
-#        bar_clr = "#00000000"
-#    elif qtile.current_layout.name == "columns":
-#        bar_clr = "#00000000"
