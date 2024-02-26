40364d0460f36bc68bc337184a009011d982e405
holykremowka2137 <victoria2137@tutanota.com>
2024-02-26 03:45:42 +0100

    added fish_git_prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,5 @@
 function fish_right_prompt
+    fish_git_prompt
     if test $CMD_DURATION
         set -g duration (echo "$CMD_DURATION 1000" | awk '{printf "%.0fs", $1 / $2}')
         if test $duration = "1s" || test $duration = "0s"
